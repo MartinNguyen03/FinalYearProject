27acf0c182d9704a67548e29e3f32aa641d4555a
Martin Nguyen <101950460+MartinNguyen03@users.noreply.github.com>
2025-06-09 18:55:35 +0100

    Delete catkin_ws/build directory

--- catkin_ws/build/yumi_moveit_config/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /catkin_ws/src/yumi-prl/yumi_moveit_config
-# Build directory: /catkin_ws/build/yumi_moveit_config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
